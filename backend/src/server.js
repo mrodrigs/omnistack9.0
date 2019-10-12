063335bcbd3bb1b9080d347e14471f6a792aa784
Mauricio <grtorres17@gmail.com>
2019-10-12 15:01:38 -0300

    frontend first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -16,7 +18,9 @@ mongoose.connect('mongodb+srv://omnistack:16965274@omnistack-34xgg.mongodb.net/s
 // req.params = Acessar route params (edição e delete)
 // req.body = Acessar corpo da requisição (criação e edição de registros)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '../', './uploads')))
 app.use(routes);
 
 app.listen(3333);
